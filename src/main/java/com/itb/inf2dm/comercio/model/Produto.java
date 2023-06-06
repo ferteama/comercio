fe6f1f72198dc0e1313ba5cebdc3d0a80347410f
rm86322 <rm86322@estudante.fieb.edu.br>
2023-06-06 09:15:52 -0300

    Tela de Produtos e model produtos parcial

--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -2,4 +2,13 @@ package com.itb.inf2dm.comercio.model;
 
 public class Produto {
 
+	private Long id;
+	private String descricao;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+	
+	//public : Modificador de acesso livre para todas as classes
+	// private: Modificador de acesso restirto apenas para membros 
+   // da própria classe. Entende-se por "membros" (atributos e/ou métodos)
 }
