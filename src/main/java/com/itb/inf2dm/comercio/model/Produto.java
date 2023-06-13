90e88eddf6cd6c5a52f346cf1420f7c702fd52d0
rm86322 <rm86322@estudante.fieb.edu.br>
2023-06-13 09:15:48 -0300

    Edição template produto e criação do novo produto

--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -11,4 +11,46 @@ public class Produto {
 	//public : Modificador de acesso livre para todas as classes
 	// private: Modificador de acesso restirto apenas para membros 
    // da própria classe. Entende-se por "membros" (atributos e/ou métodos)
+   
+	// Métodos Setter's e Getter's : Adicionar e Recuperar dados do atributo "RESPECTIVAMENTE"
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+    public void setDescricao(String descricao) {
+	    this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+	    return descricao;
+
+}
+
+    public void setNome(String nome) {
+        this.nome = nome;
+}
+
+    public String getNome() {
+        return nome;
+}    
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+}
+
+    public String getCodigoBarras() {
+        return descricao;
+}
+    public void setPreco(double preco) {
+        this.preco = preco;
+}
+
+    public double getPreco() {
+        return preco;
+}
 }
